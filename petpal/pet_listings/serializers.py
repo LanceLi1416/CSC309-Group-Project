a86f7d141b35379d99119b32646bba65993a0134
nicholaspoon03 <npoon2003@gmail.com>
2023-11-10 17:14:43 -0500

    removing comments

--- petpal/pet_listings/serializers.py
+++ petpal/pet_listings/serializers.py
@@ -1,13 +1,11 @@
 from rest_framework import serializers
-from django.core.files.storage import FileSystemStorage
 from PIL import Image
 from io import BytesIO
 from datetime import datetime
 
-import imghdr
 import os
 
-from .models import Pet, Owner, PetListing, Picture, User
+from .models import Pet, Owner, PetListing, Picture
 
 class PictureSerializer(serializers.ModelSerializer):
     class Meta:
@@ -35,9 +33,6 @@ class PetListingSerializer(serializers.Serializer):
     colour = serializers.CharField(source='pet.colour', required=True)
     vaccinated = serializers.BooleanField(source='pet.vaccinated', required=True)
     other_info = serializers.CharField(source='pet.other_info', required=False)
-    # pictures = serializers.SerializerMethodField(source='pet.pictures') # TODO: works for display
-    # pictures = serializers.ImageField(source='pet.pictures', required=True)
-    # pictures = PictureSerializer(many=True, required=False)
     pictures = serializers.ListField(child=serializers.ImageField(), source='pet.pictures.all', required=True) # TODO: works for creation
     owner_name = serializers.CharField(source='owner.name', required=True)
     email = serializers.EmailField(source='owner.email', required=True)
@@ -45,11 +40,6 @@ class PetListingSerializer(serializers.Serializer):
     location = serializers.CharField(source='owner.location', required=True)
     owner_birthday = serializers.DateField(required=True, source='owner.birthday')
     status = serializers.ChoiceField(required=False, choices=STATUS_CHOICES)
-
-    # def get_pictures(self, pet_listing):
-    #     print(1)
-    #     print(pet_listing)
-    #     return PictureSerializer(pet_listing.pet.pictures.all(), many=True).data
 
 
     def validate_vaccinated(self, vaccinated):
@@ -62,7 +52,6 @@ class PetListingSerializer(serializers.Serializer):
         pictures_data = data.get('pictures')
         if type(instance) == PetListing and pictures_data is not None:
             pictures_data = PictureSerializer(instance.pet.pictures.all(), many=True).data
-            # pictures_data = [PictureSerializer(pic).data for pic in pictures_data]
             data['pictures'] = pictures_data
             return data
         else:
@@ -76,11 +65,6 @@ class PetListingSerializer(serializers.Serializer):
         if len(pictures) > 5:
             raise serializers.ValidationError({'pictures': f'{pictures}Only a maximum of 5 pictures can be uploaded'})
         return pictures
-        
-    # def validate(self, data):
-    #     self.validate_pictures(data.get('pictures'))
-    #     self.validate_vaccinated(data.get('vaccinated'))
-    #     return data
 
     def create(self, validated_data, request):
         email = validated_data['owner']['email']
@@ -212,59 +196,3 @@ class SearchModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = PetListing
         fields = ['pet', 'owner', 'shelter', 'status', 'last_update', 'creation_date']
-
-class SearchSerializer(serializers.Serializer):
-    GENDER = [
-        ('male', 'Male'),
-        ('female', 'Female')
-    ]
-    SORT = [
-        ('pet__name', 'Name'),
-        ('pet__weight', 'Size')
-    ]
-    STATUS = [
-        ('available', 'Available'),
-        ('adopted', 'Adopted'),
-        ('pending', 'Pending'),
-        ('withdrawn', 'Withdrawn')
-    ]
-    PET_TYPE = [
-        ('dog', 'Dog'),
-        ('cat', 'Cat'),
-        ('bird', 'Bird'),
-        ('other', 'Other')
-    ]
-    # shelter_query = User.objects.filter(is_seeker=False).all()
-    # shelter_choices = []
-    # for shelter in shelter_query:
-    #     shelter_choices.append((shelter.id, shelter.username))
-    # TODO: For some reason this needs to be commented to makemigrations
-
-    name = serializers.CharField(source="pet.name")
-    gender = serializers.MultipleChoiceField(choices=GENDER, source="pet.gender") 
-    # shelter = serializers.MultipleChoiceField(choices=shelter_choices)
-    status = serializers.MultipleChoiceField(choices=STATUS)
-    pet_type = serializers.MultipleChoiceField(choices=PET_TYPE)
-    start_date = serializers.DateField()
-    end_date = serializers.DateField()
-    sort = serializers.ChoiceField(choices=SORT)
-
-    # data = []
-
-    # for p in pet_listings:
-    #     pet_entry = {
-    #         'name': p.name,
-    #         'gender': p.gender,
-    #         'birthday': p.gender,
-    #         'weight': p.weight,
-    #         'pet_type': p.animal,
-    #         'breed': p.breed,
-    #         'colour': p.colour,
-    #         'vaccinated': p.vaccinated,
-    #         'other_info': p.other_info,
-    #         'shelter': p.shelter.name,
-    #         'status': p.status,
-    #         'last_update': p.last_update,
-    #         'creation_date': p.creation_date
-    #     }
-    #     data.append(pet_entry)
