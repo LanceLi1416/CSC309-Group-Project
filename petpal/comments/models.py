f71ca0d0d4d77799c6bd3e8be7cb76d85ddc5d11
Raghav Srinivasan <rsrinivasan2021@gmail.com>
2023-11-10 22:27:45 -0500

    Changed models to allow some fields null

--- petpal/comments/models.py
+++ petpal/comments/models.py
@@ -3,16 +3,16 @@ from accounts.models import User
 from applications.models import Application
 
 class ShelterComment(models.Model):
-    shelter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='shelter_comments')
-    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    shelter = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='shelter_comments')
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     comment = models.TextField()
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, related_name="replies")
     date = models.DateTimeField(auto_now_add=True)
 
 
 class ApplicationComment(models.Model):
-    application = models.ForeignKey(Application, on_delete=models.CASCADE)
-    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    application = models.ForeignKey(Application, on_delete=models.CASCADE, null=True, related_name='application_comments')
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     comment = models.TextField()
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, related_name="replies")
     date = models.DateTimeField(auto_now_add=True)
