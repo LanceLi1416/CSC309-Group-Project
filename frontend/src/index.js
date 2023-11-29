2d121d522c23602ede7e32d23b1262fcd70d487f
Lance Li <46139702+LanceLi1416@users.noreply.github.com>
2023-11-29 04:14:47 -0500

    Finish Footer component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './index.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from './App';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import reportWebVitals from './reportWebVitals';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home />}/>
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route exact path="/" element={<Home/>}/>
+                <Route exact path="/login" element={<Login/>}/>
+                <Route exact path="/register" element={<Register/>}/>
+            </Routes>
+            <Footer/>
+        </BrowserRouter>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,24 +1,26 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Logo from '../../components/Logo';
 import Header from '../../components/Header';
 import FormField from '../../components/FormField';
-import { Link, useNavigate } from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import * as formik from 'formik';
 import * as yup from 'yup';
 import axios from 'axios'
+import {jwtDecode} from "jwt-decode";
 
 function Login() {
     const [loginError, setLoginError] = useState(null);
     const navigate = useNavigate();
+    const API_URL = process.env.REACT_APP_API_URL;
 
     const submitHandler = (values) => {
         // TODO: endpoint url
         axios({
             method: "POST",
-            url: "http://localhost:8000/api/token/",
+            url: API_URL + "api/token/",
             data: {
                 "username": values.email,
                 "password": values.password
@@ -27,6 +29,21 @@ function Login() {
             localStorage.setItem('access_token', response.data.access);
             localStorage.setItem('refresh_token', response.data.refresh);
             // console.log(localStorage.getItem('access_token'));
+
+            // Stores the user information in local storage
+            const user_id = jwtDecode(response.data.access).user_id;
+            axios({
+                method: "GET",
+                url: API_URL + "accounts/" + user_id + "/",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+                }
+            }).then((response) => {
+                localStorage.setItem('user', JSON.stringify(response.data));
+            }).catch((error) => {
+                // console.log(error.response.data.detail);
+            });
             navigate("/");
         }).catch((error) => {
             // console.log(error.response.data.detail);
@@ -34,30 +51,34 @@ function Login() {
         });
     };
 
-    const { Formik } = formik;
+    const {Formik} = formik;
     const schema = yup.object().shape({
         email: yup.string().email('Invalid email').required('Email is required'),
         password: yup.string().required('Password is required'),
     });
 
     return (<>
-        <Logo />
-        <Header header="Welcome Back to PetPal!" subheader="Login with your email address and password." />
+        <Logo/>
+        <Header header="Welcome Back to PetPal!" subheader="Login with your email address and password."/>
         <Formik
             validationSchema={schema}
             onSubmit={(values) => submitHandler(values)}
-            initialValues={{ email: '', password: '' }}
+            initialValues={{email: '', password: ''}}
         >
-            {({ handleSubmit, handleChange, values, touched, errors }) => (
-            <Form noValidate onSubmit={handleSubmit} className="centered-form mt-5">
-                <Row className="mb-3">
-                    <FormField id="email" width="12" type="email" placeholder="Email" name="email" value={values.email} handleChange={handleChange} error={touched.email && errors.email} />
-                </Row>
-                <Row className="mb-3">
-                    <FormField id="password" width="12" type="password" placeholder="Password" name="password" value={values.password} handleChange={handleChange} error={touched.password && errors.password} />
-                </Row>
-                <Button type="submit">Login</Button>
-            </Form>
+            {({handleSubmit, handleChange, values, touched, errors}) => (
+                <Form noValidate onSubmit={handleSubmit} className="centered-form mt-5">
+                    <Row className="mb-3">
+                        <FormField id="email" width="12" type="email" placeholder="Email" name="email"
+                                   value={values.email} handleChange={handleChange}
+                                   error={touched.email && errors.email}/>
+                    </Row>
+                    <Row className="mb-3">
+                        <FormField id="password" width="12" type="password" placeholder="Password" name="password"
+                                   value={values.password} handleChange={handleChange}
+                                   error={touched.password && errors.password}/>
+                    </Row>
+                    <Button type="submit">Login</Button>
+                </Form>
             )}
         </Formik>
         <div className="mt-2 text-center">
