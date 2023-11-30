b0169d1fc6388ac988ede20b763ba7d86357e8a6
Lance Li <46139702+LanceLi1416@users.noreply.github.com>
2023-11-29 23:16:26 -0500

    Add header and footer (#34)
    * Finish Footer component
    
    * Update .gitignore to exclude migrations
    
    * Finish the majority of Header component
    
    * Finish notifications
    
    * Rename
    
    * Fix for building
    
    * Fix pagination error
    
    * Perp for merge

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './index.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from './App';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import reportWebVitals from './reportWebVitals';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import Notifications from "./pages/Notifications";
+import {NotificationProvider} from "./context/NotificationContext";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home />}/>
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (<NotificationProvider>
+            <BrowserRouter>
+                <Header/>
+                <Routes>
+                    <Route exact path="/" element={<Home/>}/>
+                    <Route exact path="/login" element={<Login/>}/>
+                    <Route exact path="/register" element={<Register/>}/>
+                    <Route exact path="/notifications" element={<Notifications/>}/>
+                </Routes>
+                <Footer/>
+            </BrowserRouter>
+        </NotificationProvider>);
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode>
+    <App/>
+</React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
