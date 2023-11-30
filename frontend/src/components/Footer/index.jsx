17c2f22409d00d1e099afed24c7e9d15e824f6e2
Lance Li <46139702+LanceLi1416@users.noreply.github.com>
2023-11-29 23:13:14 -0500

    Perp for merge

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -3,7 +3,6 @@ import {Link} from "react-router-dom";
 import logo from '../../assets/images/petpal.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import './style.css';
 
 
 const Footer = () => {
--- frontend/src/components/Logo/index.jsx
+++ frontend/src/components/Logo/index.jsx
@@ -1,9 +1,9 @@
-import logo from '../../logo.svg';
+import logo from '../../assets/images/petpal.svg';
 
 function Logo() {
     return (<div className="mt-2 mb-4 d-flex flex-column align-items-center">
         <div className="col text-center image-wrapper">
-        <img src={logo} className="wrapped-img" alt="Logo" />
+            <img src={logo} className="wrapped-img" alt="Logo"/>
         </div>
     </div>);
 }
